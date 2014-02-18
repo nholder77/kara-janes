a383c050ea7b26c310d6ec8b2e21a3dd7e34314b
nholder77 <nicholas.a.holder@gmail.com>
2014-02-17 19:32:48 -0500

    Made better class names
    They're better now

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 $(document).ready(function() {
-	$('.who').click(function() {
-		$('.text_who').toggleClass('hidden');
+	$('.title_about').click(function() {
+		$('.content_about').toggleClass('hidden');
 		
-		if ($('.text_what').not('.hidden')) {
-		    $('.text_what').addClass('hidden');
+		if ($('.content_gallery').not('.hidden')) {
+		    $('.content_gallery').addClass('hidden');
 		}
 		
-		if ($('.text_much').not('.hidden')) {
-		    $('.text_much').addClass('hidden');
+		if ($('.content_prices').not('.hidden')) {
+		    $('.content_prices').addClass('hidden');
 		}	
 	});
-	$('.what').click(function() {
-		$('.text_what').toggleClass('hidden');
+	$('.title_gallery').click(function() {
+		$('.content_gallery').toggleClass('hidden');
 		
-		if ($('.text_who').not('.hidden')) {
-		     $('.text_who').addClass('hidden');
+		if ($('.content_about').not('.hidden')) {
+		     $('.content_about').addClass('hidden');
 		}
 		
-		if ($('.text_much').not('.hidden')) {
-		    $('.text_much').addClass('hidden');
+		if ($('.content_prices').not('.hidden')) {
+		    $('.content_prices').addClass('hidden');
 		}
 	});
-	$('.much').click(function() {
-		$('.text_much').toggleClass('hidden');
+	$('.title_prices').click(function() {
+		$('.content_prices').toggleClass('hidden');
 		
-		if ($('.text_who').not('.hidden')) {
-			$('.text_who').addClass('hidden');
+		if ($('.content_about').not('.hidden')) {
+			$('.content_about').addClass('hidden');
 		}
 		
-		if ($('.text_what').not('.hidden')) {
-			$('.text_what').addClass('hidden');
+		if ($('.content_gallery').not('.hidden')) {
+			$('.content_gallery').addClass('hidden');
 		}
 	});
 });
